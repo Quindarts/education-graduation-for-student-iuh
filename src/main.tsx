fb898aa5ef9de1d7f9a610d161c590e30c4db487
Quindarts <quang82thcspb@gmail.com>
2024-08-01 10:03:06 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './providers/ReactQuery';
 import App from './App';
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         >
           <QueryClientProvider client={queryClient}>
             <App />
-            <ReactQueryDevtools initialIsOpen={false} />
+            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
           </QueryClientProvider>
         </SnackbarProvider>
       </BrowserRouter>
