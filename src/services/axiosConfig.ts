c12747e12de497397979381cbf99315e92f1caed
Quindarts <quang82thcspb@gmail.com>
2024-08-09 15:11:16 +0700

    add port devops

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -2,8 +2,8 @@ import { getValueFromLocalStorage } from '@/utils/localStorage';
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-  // baseURL: `${env.BASE_URL}/api/v1`,
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: `${env.BASE_URL}/api/v1`,
+  // baseURL: 'http://localhost:3000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
