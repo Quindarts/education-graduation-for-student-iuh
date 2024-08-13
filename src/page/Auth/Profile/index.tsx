59ccee9911c20c63ffff124273009e7e54ca99db
Quindarts <quang82thcspb@gmail.com>
2024-08-13 17:19:26 +0700

    update UI

--- src/page/Auth/Profile/index.tsx
+++ src/page/Auth/Profile/index.tsx
@@ -19,10 +19,10 @@ export const validateSchemaStudent = Yup.object().shape({
       'Họ và tên chỉ bao gồm chữ cái in hoa và in thường.',
     )
     .required('Họ và tên không được để trống'),
-  phone: Yup.string()
-    .matches(/^0\d{9}$/, 'Số điện thoại phải bắt đầu bằng số 0 và gồm 10 chữ số')
-    .required('Số điện thoại không được để trống'),
-  email: Yup.string().email('Email phải hợp lệ').required('Email không được để trống'),
+  phone: Yup.string().matches(/^0\d{9}$/, 'Số điện thoại phải bắt đầu bằng số 0 và gồm 10 chữ số'),
+  // .required('Số điện thoại không được để trống'),
+  email: Yup.string().email('Email phải hợp lệ'),
+  // .required('Email không được để trống'),
 });
 
 enum EnumGender {
